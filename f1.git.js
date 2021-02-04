9772028c49b873f76c1a62788dfffa2caa71732c
Adriano Barreto <adrianooscar.mb@hotmail.com>
2021-02-04 20:07:08 -0300

    add  f1

---
+++ f1.git.js
@@ -0,0 +1,13 @@
+let button = document.createElement('button');
+button.setAttribute('id','button');
+butoon.addEventListener('click', function(){
+    document.body.style.backgroundColor = "red";
+
+})
+
+button.setAttribute('id','p');
+butoon.addEventListener('click', function(){
+    document.body.style.backgroundColor = "red";
+
+})
+
